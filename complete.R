9a33fdb1cddbf1bb9a2c3bb1d3380c2b7345a492
AlienBaby <matt.j.warren@gmail.com>
2016-04-03 00:25:43 +0100

    cleaning up a  bit

--- complete.R
+++ complete.R
@@ -1,8 +1,12 @@
 complete <- function(directory="specdata",id=1:332){
+  
   #files for datasets
   filenames=file.path(directory,list.files(directory))[id]
+  
   #dataframes for the datasets
   frames=lapply(filenames,read.csv)
+  
+  #get number of complete cases per frame
   df=data.frame(id=as.integer(sub(".csv","",basename(filenames))),
                 nobs=unlist(
                   lapply(
--- corr.R
+++ corr.R
@@ -1,15 +1,23 @@
+source(complete)
+source()
 corr <- function(directory="specdata",threshold=0){
+  
   #complete cases per dataset
   ds_c=complete(directory)
+  
   #datasets with >threshold complete cases (id col is file index)
   ds_t=ds_c[ds_c$nobs>threshold,]
+  
   #files for those datasets
   ds_filenames=file.path(directory,list.files(directory))[ds_t$id]
+  
   #dataframes for those datasets
   frames=lapply(ds_filenames,read.csv)
+  
   #get vector of correlations between sulfate and nitrate
   #for the dataframes
   unlist(
     lapply(frames,function(x) cor(x["sulfate"],x["nitrate"],
                                   use="complete.obs")))
+  
 }
